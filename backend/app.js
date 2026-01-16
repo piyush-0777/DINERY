4b94a66482a9e8c1f454a69bad58dd133a68abd5
piyush-0077 <khandharpiyush5@gmail.com>
2026-01-16 20:54:32 +0530

    make js file for routes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,18 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 const mongoDB_connection = require('./config/mongoDB-connection')
-
+const path = require('path')
+const cookieParser = require('cookie-parser')
 const app = express()
 
 
 app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+app.use("/api/tables" , require('./'))
 
 app.get('/' , (req , res)=>{
     console.log('express is working....')
