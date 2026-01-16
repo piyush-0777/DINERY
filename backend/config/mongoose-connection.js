7a91e3a76a12054f82362d1d7418278e853de0ab
piyush-0077 <khandharpiyush5@gmail.com>
2026-01-16 15:25:13 +0530

    add mongoDB connection

---
+++ backend/config/mongoose-connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URL
+
+mongoose.connect(`${url}/dinery`)
+.then(()=>{
+    console.log('mongoDB is connected....')
+})
+.catch((e)=>{
+    console.log('mongoDB connection error' , e)
+})
+
+module.exports = mongoose.connection;
