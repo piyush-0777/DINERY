422e0451fcc68bd4191958b44d8a74fe7fed2820
piyush-0077 <khandharpiyush5@gmail.com>
2026-01-29 16:53:38 +0530

    make orderController function

--- backend/controllers/tableController.js
+++ backend/controllers/tableController.js
@@ -1,33 +1,130 @@
-  const tableModel = require('../models/table-model')
-  const generateQR = require('../utils/generateQR')
-  const crypto = require('crypto')
+const tableModel = require('../models/table-model')
+const generateQR = require('../utils/generateQR')
+const crypto = require('crypto')
 
-  exports.createTable = async (req , res) => {
-    try{
-    const restaurant = req.restaurant
-    const {capacity, tableNumber} = req.body
-    if(!restaurant) {
-        res.status(404).json({
-            error: 'restaurant is not found'
-        })
-    }
-    
-    const table = await tableModel.create({
-        restaurant:restaurant._id ,
-        tableId:tableNumber ,
-        capacity:capacity ,
-        qrCode: crypto.randomUUID()
-         })
-
-         const qrImage = await generateQR(table , restaurant.name)
-         return res.status(200).json({
-            table,
-            qrImage
-         })
-        } catch (error) {
-            console.log(error)
-            res.status(500).json({
-                error: 'internal server error'
+exports.getAllTable = async (req, res) => {
+    try {
+        const restaurant = req.restaurant
+        if (!restaurant) {
+            res.status(404).json({
+                error: 'restaurant is not found'
             })
         }
-  }
+
+        const tables = await tableModel.find({ restaurant: restaurant._id })
+        let tablesWithQrimage = [];
+        for (let table of tables) {
+            const qrImage = await generateQR(table, restaurant.name)
+            tablesWithQrimage.push({ ...tables, qrImage: qrImage })
+        }
+        return res.status(200).json({
+            tables: tablesWithQrimage
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: 'internal server error'
+        })
+    }
+}
+
+exports.createTable = async (req, res) => {
+    try {
+        const restaurant = req.restaurant
+        const { capacity, tableNumber } = req.body
+        if (!restaurant) {
+            res.status(404).json({
+                error: 'restaurant is not found'
+            })
+        }
+
+        const table = await tableModel.create({
+            restaurant: restaurant._id,
+            tableId: tableNumber,
+            capacity: capacity,
+            qrCode: crypto.randomUUID()
+        })
+
+        const qrImage = await generateQR(table, restaurant.name)
+        return res.status(200).json({
+            table,
+            qrImage
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: 'internal server error'
+        })
+    }
+}
+
+exports.deleteTable = async (req, res) => {
+    try {
+        const restaurant = req.restaurant
+
+        const tableId = req.params.tableId;
+        if (!restaurant) {
+            res.status(404).json({
+                error: 'restaurant is not found'
+            })
+        }
+        const table = tableModel.findOne(tableId)
+        if (!table) {
+            res.status(404).json({
+                error: 'table is not found'
+            })
+        }
+        if (table.status !== "available") {
+            return res.status(400).json({
+                error: 'table cannot be deleted while occupied or reserved',
+            })
+        }
+        await table.deleteOne();
+        return res.status(200).json({
+            message: 'table is deleted..',
+            table
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: 'internal server error'
+        })
+    }
+
+}
+
+exports.updateStatus = async (req, res) => {
+    try {
+        const restaurant = req.restaurant
+        const { status } = req.body
+        if (!restaurant) {
+            return res.status(404).json({
+                error: 'restaurant is not found'
+            })
+        }
+        const tableId = req.params.tableId;
+        if (!table) {
+            return res.status(404).json({
+                error: 'table is not found'
+            })
+        }
+        const table = tableModel.findOne(tableId)
+        if (!table) {
+            return res.status(404).json({
+                error: 'table is not found'
+            })
+        }
+        table.status = status;
+        await table.save()
+        return res.status(200).json({
+            message: 'table status is updated.',
+            table
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            error: 'internal server error'
+        })
+    }
+}
--- backend/routes/tableRoutes.js
+++ backend/routes/tableRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express')
-const {createTable} = require('../controllers/tableController')
+const {createTable , deleteTable , updateStatus} = require('../controllers/tableController')
 const {authenticateResturant} = require('../middlewares/authMiddleware')
 const router = express.Router();
 
-router.post('/createtable',authenticateResturant , createTable);
+router.get('/getalltable' ,authenticateResturant, )
+router.post('/createtable',authenticateResturant , createTable)
+router.delete('/deletetable/:tableId' ,authenticateResturant, deleteTable )
+router.put('/updatetablestatus/:tableId',authenticateResturant ,updateStatus )
+
 
 module.exports = router
