0396f0b0d431fb6ff52b710cdbe304c7961b64ce
piyush-0077 <khandharpiyush5@gmail.com>
2026-01-31 23:43:22 +0530

    edit orderCard

--- backend/utils/generateQR.js
+++ backend/utils/generateQR.js
@@ -3,7 +3,7 @@ const QRCode = require('qrcode')
 
 const generateQR =async (table , res_name) =>{
     try {
-    const qrUrl = `http://10.102.40.29:5173/customer/${res_name}/login/?token=${table.qrCode}`
+    const qrUrl = `${process.env.FRONTEND_URL}/customer/${res_name}/login/?token=${table.qrCode}`
     const qrImage = await QRCode.toDataURL(qrUrl);
     return qrImage
     } catch (error) {
