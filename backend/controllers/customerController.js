9ae378e0148b71687139f8571e0f3b27abccdf38
piyush-0077 <khandharpiyush5@gmail.com>
2026-02-01 23:16:56 +0530

    change in customer login api

--- backend/controllers/customerController.js
+++ backend/controllers/customerController.js
@@ -1,22 +1,25 @@
 
 const customer = require('../models/customer-model')
+const restaurantModel = require('../models/restaurant-model')
 
 exports.customerLogin = async (req , res) =>{
 const {name , phone} = req.body
-const {restaurantId } = req.params;
+const {restaurantName } = req.params;
 console.log("Original URL:", req.originalUrl);
 console.log("Path:", req.path);
 console.log("Method:", req.method);
 console.log('res id',req.params)
-
+console.log(restaurantName)
+const restaurant = await restaurantModel.findOne({restaurantName})
+console.log(restaurant)
 const loginCustomer = await customer.create({
-    restaurant:restaurantId ,
+    restaurant:restaurant._id,
     name ,
     phone,
 })
 
 if(loginCustomer) {
-    res.status(200).json({message: 'loged in.'})
+    res.status(200).json({message: 'loged in.' , data:loginCustomer})
 } else {
     res.status(400).json({error: 'error in login'})
 }
--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -2,6 +2,6 @@ const express = require('express')
 const router = express.Router()
 const {customerLogin} = require('../controllers/customerController')
 
-router.post('/:restaurantId/login' , customerLogin)
+router.post('/:restaurantName/login' , customerLogin)
 
 module.exports = router;
